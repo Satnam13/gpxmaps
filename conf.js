808b11495133c81852d6076e734d2b6b9c6d0269
Satnam13 <47239909+Satnam13@users.noreply.github.com>
2019-02-01 13:43:03 +0530

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,4 @@
+module.exports.database = 'mongodb://Satnam:imsingh123@ds115753.mlab.com:15753/travel-journal',
+module.exports.secret = 'my-secret';
+//mongodb://<dbuser>:<dbpassword>@ds115753.mlab.com:15753/travel-journal
+//mongodb://localhost:27017/travel-journal
